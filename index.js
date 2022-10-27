3711616fdf6ced9ab47e5059deff8ea4b00b0526
ArkanKovanAbdullah <79310452+ArkanKovanAbdullah@users.noreply.github.com>
2022-10-28 01:17:07 +0300

    lets see

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ class Collect {
     booksContainer.innerHTML = '';
     for (let i = 0; i < this.books.length; i += 1) {
       const book = document.createElement('li');
+      book.className = 'book-list';
       book.innerHTML = `<span class="title"> "${this.books[i].title}" by ${this.books[i].author}</span>`;
       const btn = document.createElement('button');
       btn.className = 'list-btn';
@@ -59,3 +60,28 @@ document.forms[0].onsubmit = (event) => {
   window.localStorage.setItem('storedBooks', JSON.stringify(bookie.books));
 };
 
+const listOfBooksOnScreen = document.getElementById('list');
+const addNewBookForm = document.getElementById('form-section');
+const listBtn = document.getElementById('list-btn');
+const addNewBtn = document.getElementById('add-new-btn');
+const contact = document.getElementById('contact');
+const contactBtn = document.getElementById('contact-btn');
+const addBookForm = document.querySelector('books-form');
+
+listBtn.addEventListener('click', () =>{
+  listOfBooksOnScreen.classList.remove('display-none');
+  addNewBookForm.classList.add('display-none');
+  contact.classList.add('display-none')
+})
+
+addNewBtn.addEventListener('click', () =>{
+  listOfBooksOnScreen.classList.add('display-none');
+  addNewBookForm.classList.remove('display-none');
+  contact.classList.add('display-none')
+})
+
+contactBtn.addEventListener('click', () =>{
+  listOfBooksOnScreen.classList.add('display-none');
+  addNewBookForm.classList.add('display-none');
+  contact.classList.remove('display-none')
+})
